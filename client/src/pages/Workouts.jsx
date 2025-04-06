8a2751f91ef2d2f172a880473fef5a1ff6807105
tyoma <tvtyyiovsv@gmail.com>
2025-04-07 00:09:18 +0200

    complete dashboard page

---
+++ client/src/pages/Workouts.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Workouts = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Title>Select Date</Title>
+        </Left>  
+        <Right>
+          
+        </Right>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Workouts
